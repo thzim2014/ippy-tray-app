2c94fc453779c397e62ba767cd810aed393e1910
GoblinRules <34421482+GoblinRules@users.noreply.github.com>
2025-08-06 16:08:22 +0100

    Delete run.ps1

--- run.ps1
+++
@@ -1,3 +0,0 @@
-# Download and run the installer .bat from GitHub
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/GoblinRules/ippy-tray-app/main/install_ippy.bat" -OutFile "$env:TEMP\install_ippy.bat"
-Start-Process -FilePath "$env:TEMP\install_ippy.bat"
